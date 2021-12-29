8fd77b3e75ffe074c0bc3fe298d6acea127d76a5
Abebe Tekle Abrha <abe.habesha@gmail.com>
2021-12-28 19:21:44 -0600

    first commit

---
+++ src/main/java/com/example/jenkinsproject/JenkinsProjectApplication.java
@@ -0,0 +1,15 @@
+package com.example.jenkinsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
+
+@SpringBootApplication
+public class JenkinsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsProjectApplication.class, args);
+    }
+
+}
